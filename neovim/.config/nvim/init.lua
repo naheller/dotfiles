69e944ed38fcc25db29985e9e8cb697a6ac3f619
Nathan Heller <gritty.desk3330@fastmail.com>
2025-06-30 12:44:37 -0400

    add waybar style and misc other

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -6,3 +6,13 @@ o.shiftwidth = 4
 -- o.expandtab = true -- Works without this
 
 o.number = true
+
+-- Rebind split buffer navigation
+vim.keymap.set("n", "<C-J>", "<C-W><C-J>")
+vim.keymap.set("n", "<C-K>", "<C-W><C-K>")
+vim.keymap.set("n", "<C-L>", "<C-W><C-L>")
+vim.keymap.set("n", "<C-H>", "<C-W><C-H>")
+
+-- Default splits to open to the right and bottom
+o.splitright = true
+o.splitbelow = true
